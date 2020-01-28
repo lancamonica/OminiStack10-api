1152879560b8f0270915a4342b525753271ede8b
unknown <mo_silva1015@icloud.com>
2020-01-28 14:45:54 -0300

    feat: creating longitude and latitude

--- src/routs.js
+++ src/routs.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const axios = require("axios");
+const Dev = require("./models/dev");
 
 const routes = Router();
 
 routes.post("/devs", async (req, ress) => {
-  const { github_username, techs } = req.body;
+  const { github_username, techs, latitude, longitude } = req.body;
   const response = await axios.get(
     `https://api.github.com/users/${github_username}`
   );
@@ -12,9 +13,22 @@ routes.post("/devs", async (req, ress) => {
   const { name = login, avatar_url, bio } = response.data;
 
   const techsArray = techs.split(",").map(tech => tech.trim());
-  console.log(name, avatar_url, bio, github_username);
 
-  return ress.json({ message: "teste" });
+  const location = {
+    type: "Point",
+    coordinates: [longitude, latitude]
+  };
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray,
+    location
+  });
+
+  return ress.json(dev);
 });
 
 module.exports = routes;
