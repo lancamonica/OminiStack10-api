5fead6250f74f3d820da93daaa872fdb8baabc47
unknown <mo_silva1015@icloud.com>
2020-01-28 12:21:05 -0300

    Feat: make models

--- src/routs.js
+++ src/routs.js
@@ -1,10 +1,19 @@
 const { Router } = require("express");
+const axios = require("axios");
 
 const routes = Router();
 
-routes.post("/users", (req, ress) => {
-  const corpo = req.body;
-  console.log(corpo.cpf);
+routes.post("/devs", async (req, ress) => {
+  const { github_username, techs } = req.body;
+  const response = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+
+  const { name = login, avatar_url, bio } = response.data;
+
+  const techsArray = techs.split(",").map(tech => tech.trim());
+  console.log(name, avatar_url, bio, github_username);
+
   return ress.json({ message: "teste" });
 });
 
