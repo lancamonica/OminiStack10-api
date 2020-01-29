4a0a914e550d2d91e6754a2901ba4691617d83e0
unknown <mo_silva1015@icloud.com>
2020-01-28 21:19:18 -0300

    feat: make search

--- src/controllers/devControllers.js
+++ src/controllers/devControllers.js
@@ -1,30 +1,42 @@
 const axios = require("axios");
 const Dev = require("../models/dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
+  async index(req, ress) {
+    const devs = await Dev.find();
+
+    return ress.json(devs);
+  },
+
   async store(req, ress) {
     const { github_username, techs, latitude, longitude } = req.body;
-    const response = await axios.get(
-      `https://api.github.com/users/${github_username}`
-    );
 
-    const { name = login, avatar_url, bio } = response.data;
+    let dev = await Dev.findOne({ github_username });
 
-    const techsArray = techs.split(",").map(tech => tech.trim());
+    if (!dev) {
+      const response = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
 
-    const location = {
-      type: "Point",
-      coordinates: [longitude, latitude]
-    };
+      const { name = login, avatar_url, bio } = response.data;
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+      const techsArray = parseStringAsArray(techs);
+
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
+    }
 
     return ress.json(dev);
   }
--- src/routs.js
+++ src/routs.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const DevController = require("./controllers/devControllers");
+const SearchController = require("./controllers/searchController");
 
 const routes = Router();
 
+routes.get("/devs", DevController.index);
 routes.post("/devs", DevController.store);
+
+routes.get("/search", SearchController.index);
 
 module.exports = routes;
